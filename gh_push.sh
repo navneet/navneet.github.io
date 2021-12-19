e61cc108d3a61a32b5cf1e17d156596abce2947d
Navneet <navneet@kalabaaz.com>
2021-12-20 03:22:25 +0530

    Automated Deploy from workout-app

---
+++ gh_push.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Automates the publishing of this folder
+# to navneet.github.io
+git add .
+git commit -m "Automated Deploy from workout-app"
+git push
+
--- internal/pages/index.svelte-32ed7ed3.js
+++
@@ -1,5 +0,0 @@
-import{S as i,i as r,s as l,t as c,J as d,d as a,g as h,f as u,I as o}from"../chunks/vendor-77537132.js";function m(n){let t;return{c(){t=c(`
-
-Coming Sooooon!`),this.h()},l(e){d('[data-svelte="svelte-16ixfh2"]',document.head).forEach(a),t=h(e,`
-
-Coming Sooooon!`),this.h()},h(){document.title="Kalabaaz Workouts"},m(e,s){u(e,t,s)},p:o,i:o,o,d(e){e&&a(t)}}}class _ extends i{constructor(t){super();r(this,t,null,m,l,{})}}export{_ as default};
